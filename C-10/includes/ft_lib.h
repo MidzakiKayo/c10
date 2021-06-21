2947b21e57a86b6162a20e2dd92e1ccc605868e7
Roman <s>
2021-06-21 10:28:33 +0300

    for del

---
+++ C-10/includes/ft_lib.h
@@ -0,0 +1,11 @@
+#ifndef FT_LIB_H
+# define FT_LIB_H
+# include <unistd.h>
+
+void	ft_display_file(int argc, char **argv);
+int	ft_strcmp(char *s1, char *s2);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+
+#endif
+
